a539abae3c6e5cb877a4f154c181fc801b7cec3d
MosNes <7hekarl@gmail.com>
2023-02-26 12:53:13 -0500

    set up .env file and defined env parameters

--- functions/index.js
+++ functions/index.js
@@ -1,12 +1,13 @@
 const functions = require("firebase-functions");
+const { defineInt, defineString } = require('firebase-functions/params');
 
-// // Create and deploy your first functions
-// // https://firebase.google.com/docs/functions/get-started
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+//import nodemailer
+const nodemailer = require('nodemailer');
+
+//define env parameters
+const recipient = defineString('EMAIL_RECIPIENT');
+const user = defineString('APP_USER');
+const password = defineString('APP_PASSWORD');
 
 exports.sendMail = functions.https.onRequest( (req, res) => {
     const data = req.body;
