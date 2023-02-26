af30edd2fcff762b37ea728e383c0b0236b7d480
MosNes <7hekarl@gmail.com>
2023-02-26 11:32:24 -0500

    updated sendMail function for testing

--- functions/index.js
+++ functions/index.js
@@ -1,11 +1,14 @@
 // The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
 const functions = require('firebase-functions');
 
+//import nodemailer
+const nodemailer = require('nodemailer');
+
 // define cloud function called sendMail to trigger on HTTPS request
 exports.sendMail = functions.https.onRequest(async (req, res) => {
 
     //get body of post request
     const data = req.body;
 
-    
+    res.json(data);
 })
